3006541df6eea0dc95b236ee35d8734ff7f72ea9
nbidal18 <nizarbidal18@gmail.com>
2024-02-28 13:07:42 +0100

    a

--- utils/print_ptr.c
+++ utils/print_ptr.c
@@ -6,7 +6,7 @@
 /*   By: nbidal <nbidal@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/27 15:06:01 by nbidal            #+#    #+#             */
-/*   Updated: 2024/02/28 10:28:13 by nbidal           ###   ########.fr       */
+/*   Updated: 2024/02/28 12:56:06 by nbidal           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,7 @@ int	print_ptr(void *p)
 	if (p == NULL || p == 0)
 		return (write(1, "0x0", 3));
 	else
-	{		
+	{
 		ptr_value = (uintptr_t)p;
 		return (print_nbr(ptr_value, 16, 1));
 	}
